3b0369691e4bb9d95cdef3e32054a7eb95d020c9
PARK GI-PYO <gipyopark@gmail.com>
2022-12-12 23:39:17 +0900

    #8 - 회원 관리 웹 MVC 개발
    - 홈 화면 개발
    - 회원 등록
    - 회원 목록 조회

--- src/main/java/com/example/exhellospring/controller/MemberController.java
+++ src/main/java/com/example/exhellospring/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.example.exhellospring.controller;
 
+import com.example.exhellospring.domain.Member;
 import com.example.exhellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -12,6 +17,40 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String newMember() {
+        return "members/newMember";
+    }
+
+    /**
+
+     @PostMapping("/members/new")
+     public String joinMember(@RequestParam(name = "name") String name) {
+         Member member = new Member();
+         member.setName(name);
+         memberService.join(member);
+
+         return "redirect:/";
+     }
+
+     // RequestParam 많을 경우, 별도 클래스로 전달 받아 처리
+
+     */
+    @PostMapping("/members/new")
+    public String joinMember(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String listMember(Model model) {
+        model.addAttribute("members", memberService.findMembers());
+        return "members/listMember";
     }
 
 }
