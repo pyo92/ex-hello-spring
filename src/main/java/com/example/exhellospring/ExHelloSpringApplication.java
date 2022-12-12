018b8bd02bceb3c9d9ec4157305c3c6b6f7337ed
PARK GI-PYO <gipyopark@gmail.com>
2022-12-12 11:01:24 +0900

    #1 - 프로젝트 환경설정
    - 프로젝트 생성
    - 라이브러리 의존성 확인
    - Welcome page 작성
    - thymeleaf 템플릿 엔진 동작 확인

---
+++ src/main/java/com/example/exhellospring/ExHelloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.exhellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExHelloSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExHelloSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exhellospring/ExHelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exhellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExHelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
